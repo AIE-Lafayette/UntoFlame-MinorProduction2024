0d7829a358f0bbf45769caf3d86067bdac939d87
Galaxy <bryon.guidry1@gmail.com>
2023-05-03 16:52:57 -0500

    Added base for enemy shooter behavior and remove object pooling.

---
+++ Assets/Scripts/EnemyBehavior/EnemyShooterBehavior.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooterBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bullet;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

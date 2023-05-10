e618b9dbda1c4c88463dff4a4fb638a421694d60
Galaxy <bryon.guidry1@gmail.com>
2023-05-10 16:51:41 -0500

    Added projectile spawning and enemy shooter behavior with object pooling

--- Assets/Scripts/Spawning/ProjectileSpawnerBehavior.cs
+++ Assets/Scripts/Spawning/ProjectileSpawnerBehavior.cs
@@ -1,30 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ObjectPool;
 
 public class ProjectileSpawnerBehavior : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField, Tooltip("The projectile that is fired towards the player.")]
     private GameObject _projectile;
-    [SerializeField]
+    [SerializeField, Tooltip("The speed at which the projectile moves in units per frame.")]
     private float _projectileSpeed;
-    [SerializeField]
-    private float _projectileLifetime;
-    [SerializeField]
+    [SerializeField, Tooltip("How often the bullet should spawn in seconds. Does not apply if auto fire is disabled.")]
     private float  _projectileSpawnRate;
 
+    [SerializeField, Tooltip("If enabled, the projectile will fire automatically.")]
+    private bool _shouldAutoFire;
 
-    private float _elapsedTime;
-    void Update()
+    public bool ShouldAutoFire
     {
-        _elapsedTime += Time.deltaTime;
-        if (_elapsedTime >= _projectileSpawnRate)
+        get { return _shouldAutoFire; }
+        set { _shouldAutoFire = value; }
+    }
+
+    public void Fire()
+    {
+        GameObject projectile = ObjectPoolBehavior.Instance.GetObject(_projectile, transform.position, transform.rotation);
+        Vector3 direction = (transform.position - GameManager.Instance.Player.transform.position).normalized;
+        projectile.GetComponent<Rigidbody>().velocity = (direction*-1) * _projectileSpeed;
+    }
+
+    float elapsedTime;
+    private void Update()
+    {
+        if (!_shouldAutoFire) return;
+
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= _projectileSpawnRate)
         {
-            _elapsedTime = 0;
-            GameObject projectile = Instantiate(_projectile, transform.position, transform.rotation);
-            Vector3 direction = (transform.position - GameManager.Instance.Player.transform.position).normalized;
-            projectile.GetComponent<Rigidbody>().velocity = (direction*-1) * _projectileSpeed;
-            Destroy(projectile, _projectileLifetime);
+            elapsedTime = 0;
+            Fire();
         }
     }
 }

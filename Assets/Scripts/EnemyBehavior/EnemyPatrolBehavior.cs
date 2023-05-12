ef767e2a2402511ec69f434bca933e0db8b7c482
Galaxy <bryon.guidry1@gmail.com>
2023-05-12 09:48:18 -0500

    Added object pooling behavior to bullets, having them despawn.

--- Assets/Scripts/EnemyBehavior/EnemyPatrolBehavior.cs
+++ Assets/Scripts/EnemyBehavior/EnemyPatrolBehavior.cs
@@ -1,45 +1,48 @@
 using UnityEngine;
 
-public class EnemyPatrolBehavior : MonoBehaviour
+namespace EnemyBehavior
 {
-
-	[SerializeField, Tooltip("How fast the enemy should move between each point.")]
-	private float _speed;
-
-	[SerializeField, Tooltip("A collection of patrol points. The enemy will travel in the order of patrol points from top to bottom.")]
-	private Transform[] _patrolPoints;
-
-	[SerializeField, Tooltip("The amount of time in seconds to wait before moving on to the next patrol point.")]
-	private float _waitTime;
-	private float _timeWaited;
-
-	private int _currentPoint = 0;
-
-	private Vector3 _target;
-
-	void Start()
+	public class EnemyPatrolBehavior : MonoBehaviour
 	{
-		for (int i = 0; i < _patrolPoints.Length; i++)
-			_patrolPoints[i].SetParent(null);
 
-		transform.position = _patrolPoints[0].position;
-		_target = _patrolPoints[1].position;
-	}
+		[SerializeField, Tooltip("How fast the enemy should move between each point.")]
+		private float _speed;
 
-	void FixedUpdate()
-	{
-		transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+		[SerializeField, Tooltip("A collection of patrol points. The enemy will travel in the order of patrol points from top to bottom.")]
+		private Transform[] _patrolPoints;
 
-		// check the distance to see if its close enough to move to the next point
-		if (Vector3.Distance(transform.position, _target) < 0.05f)
+		[SerializeField, Tooltip("The amount of time in seconds to wait before moving on to the next patrol point.")]
+		private float _waitTime;
+		private float _timeWaited;
+
+		private int _currentPoint = 0;
+
+		private Vector3 _target;
+
+		void Start()
 		{
-			_timeWaited += Time.deltaTime;
-			if (_timeWaited > _waitTime)
+			for (int i = 0; i < _patrolPoints.Length; i++)
+				_patrolPoints[i].SetParent(null);
+
+			transform.position = _patrolPoints[0].position;
+			_target = _patrolPoints[1].position;
+		}
+
+		void FixedUpdate()
+		{
+			transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
+			// check the distance to see if its close enough to move to the next point
+			if (Vector3.Distance(transform.position, _target) < 0.05f)
 			{
-				_currentPoint = (_currentPoint >= _patrolPoints.Length - 1) ? 0 : _currentPoint + 1;
-				_target = _patrolPoints[_currentPoint].position;
-				_timeWaited = 0;
-			}
-		}   
+				_timeWaited += Time.deltaTime;
+				if (_timeWaited > _waitTime)
+				{
+					_currentPoint = (_currentPoint >= _patrolPoints.Length - 1) ? 0 : _currentPoint + 1;
+					_target = _patrolPoints[_currentPoint].position;
+					_timeWaited = 0;
+				}
+			}   
+		}
 	}
 }
--- Assets/Scripts/Spawning/ProjectileSpawnerBehavior.cs
+++ Assets/Scripts/Spawning/ProjectileSpawnerBehavior.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using ObjectPool;
+using Utility.ObjectPool;
 
 public class ProjectileSpawnerBehavior : MonoBehaviour
 {

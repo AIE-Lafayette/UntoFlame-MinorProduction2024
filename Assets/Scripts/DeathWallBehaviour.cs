7a547b6f81ca3befd7d4da20f9314312dc57f6e1
Ryan <s228032@students.aie.edu>
2023-05-31 14:44:32 -0500

    Added GameManager to scene

--- Assets/Scripts/DeathWallBehaviour.cs
+++ Assets/Scripts/DeathWallBehaviour.cs
@@ -8,7 +8,7 @@ public class DeathWallBehaviour : MonoBehaviour
     {
         if (other.tag == "Player")
         { 
-            Destroy(other.gameObject);
+            other.GetComponent<DamageBehavior>().Kill();
         }
     }
 }

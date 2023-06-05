4adb35ee3f42301ae8579d473513f716502a8449
Ryan <s228032@students.aie.edu>
2023-06-05 14:53:36 -0500

    Adjusted CameraControllingBehaviour
    Added onDeathListener to stop camera movement when Player dies. Score is determined by camera movement.

--- Assets/Scripts/UI/CameraControllingBehaviour.cs
+++ Assets/Scripts/UI/CameraControllingBehaviour.cs
@@ -8,10 +8,25 @@ public class CameraControllingBehaviour : MonoBehaviour
 
     [SerializeField, Tooltip("The rigidbody of the gameObject that this script will be attached to.")] private Rigidbody _rigidBody;
 
+    private bool _shouldCameraMove = true;
+
     // Update is called once per frame
     void Update()
     {
+
+        if(!_shouldCameraMove)
+        {
+            return;
+        }
+
         //Move the camera body to the right of the screen.
         _rigidBody.velocity = new Vector3(speed, 0, 0);
+
+        GameManager.Instance.Score.Value = System.Convert.ToInt32(transform.localPosition.x);
+
+        GameManager.Instance.Player.GetComponent<DamageBehavior>().AddDeathEventListener(_ =>
+        {
+            _shouldCameraMove = false;
+        });
     }
 }

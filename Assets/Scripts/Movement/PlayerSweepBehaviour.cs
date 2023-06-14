a279a49cc459cda7dfec9d843d629cb1cf272c6c
Sam <semiddleton1@live.com>
2023-06-14 10:35:19 -0500

    Test

--- Assets/Scripts/Movement/PlayerSweepBehaviour.cs
+++ Assets/Scripts/Movement/PlayerSweepBehaviour.cs
@@ -6,39 +6,66 @@ using UnityEngine;
 [RequireComponent(typeof(DamageBehavior))]
 public class PlayerSweepBehaviour : MonoBehaviour
 {
-    private PlayerMovementBehaviour _moveBehaviour;
-
-    private float _invincibilityTime;
-    private float _elapsedTime = 0;
+    private GameObject _sweepArea = default;
     
     public bool IsSweeping = false;
+
+    private float _timeToSweep = 0.25f;
+    private float _timer = 0f;
+
+    //private PlayerMovementBehaviour _moveBehaviour;
+
+    //private float _invincibilityTime;
+    //private float _elapsedTime = 0;
+    
+    
 
     DamageBehavior _damageBehavior;
 
     private void Start()
     {
-        _damageBehavior = GetComponent<DamageBehavior>();
+        _sweepArea = transform.GetChild(0).gameObject;
+
+        //_damageBehavior = GetComponent<DamageBehavior>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if(Input.GetKeyDown(KeyCode.J))
         {
-            IsSweeping = true;
+            Sweep();
         }
 
-        if(IsSweeping == true)
+        if(IsSweeping)
         {
-            _damageBehavior.SetIsInvincible(true);
+            _timer += Time.deltaTime;
 
-            _elapsedTime += Time.deltaTime;
-
+            if(_timer >= _timeToSweep)
+            {
+                _timer = 0f;
+                IsSweeping = false;
+                _sweepArea.SetActive(IsSweeping);
+            }
         }
-        if(_elapsedTime >= _invincibilityTime)
-        {
-            _damageBehavior.SetIsInvincible(false);
 
-            IsSweeping = false;
-        }
+        //if(IsSweeping == true)
+        //{
+        //    _damageBehavior.SetIsInvincible(true);
+
+        //    _elapsedTime += Time.deltaTime;
+
+        //}
+        //if(_elapsedTime >= _invincibilityTime)
+        //{
+        //    _damageBehavior.SetIsInvincible(false);
+
+        //    IsSweeping = false;
+        //}
+    }
+
+    private void Sweep()
+    {
+        IsSweeping = true;
+        _sweepArea.SetActive(IsSweeping);
     }
 }
